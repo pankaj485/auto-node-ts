6651540e129ac9e8bc5f32f57826451a21251177
pankaj485 <pankajsamar2@gmail.com>
2024-02-16 17:48:33 +0530

    feat: remove git tracking, read project name in same line

--- node-ts-setup.bash
+++ node-ts-setup.bash
@@ -1,25 +1,25 @@
-#! /bin/bash 
+#! /bin/bash
 
-## setting up repository to clone 
+## setting up repository to clone
 GIT_REPO=https://github.com/pankaj485/ts-express-starter.git
 GIT_WORK_DIR=ts-express-starter
 
-# echo "-------------  removing exisitng work directory"
-# rm -rf $WORK_DIR
+## Get project name
+read -p "Project name: " USR_WORK_DIR
 
-echo "-------------  project name: "
-read USR_WORK_DIR 
-
-echo "-------------  clonning $GIT_REPO"
+echo ":::::::::::::::::  Setting up Typescript NodeJs server  :::::::::::::::::"
+echo ":::::::::::::::::  Clonning $GIT_REPO  :::::::::::::::::"
 git clone $GIT_REPO
-
 mv $GIT_WORK_DIR $USR_WORK_DIR
 
-
-echo "-------------  installing dependencies"
 cd $USR_WORK_DIR
+
+echo "::::::::::::::::: Removing git tracking  :::::::::::::::::"
+rm -rf .git
+rm .gitignore
+
+echo ":::::::::::::::::  installing dependencies  :::::::::::::::::"
 npm install
 
-echo "-------------  dependencies installed"
-echo "-------------  project is ready to use"
-echo "-------------  pleae refer package.json file for more info"
+echo ":::::::::::::::::  Project setup successfully  :::::::::::::::::"
+echo ":::::::::::::::::  pleae refer package.json file for more info  :::::::::::::::::"
